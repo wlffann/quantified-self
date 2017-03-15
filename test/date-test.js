700b383e4aa9684756acea81d684882845e40f3d
David Knott <david1k1nott@gmail.com>
2017-03-15 15:07:59 -0600

    pass user can see todays date test
    141617749
    
    I'm testing for todays date as a baseline for our future date tests.

--- test/date-test.js
+++ test/date-test.js
@@ -5,9 +5,9 @@ describe("#date-carousel", function() {
     $ = document.getElementById("diary-frame").contentWindow.$;
   });
 
-  context("When a use clicks on the date foreward button", function(){
-    it ("their diary moves a day foreward", function(){
-      var actualCurrentDate = $("#current-date").text();
+  context("When a user visits the index page", function(){
+    it ("they see todays date", function(){
+      var actualCurrentDate = $(".current-date").text().trim();
       var expectedCurrentDate = moment().format("MMMM Do, YYYY");
       assert.equal(actualCurrentDate, expectedCurrentDate)
     });
