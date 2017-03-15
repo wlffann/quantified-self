449b33e55658f5823d11488682e898ae94de4dc6
Ann Wolff <annwolffe@gmail.com>
2017-03-15 13:45:14 -0600

    Add total calories calculation
    [#141617753]

---
+++ test/test-helper.js
@@ -0,0 +1,6 @@
+function createMealDay() {
+  var mealDay = {}
+  mealDay.breakfast = [{"name": "banana", "calories": "100"}, {"name": "apple", "calories": "10"}, {"name": "ham", "calories": "26"}]
+  mealDay.lunch = [{"name": "banana", "calories": "100"}, {"name": "apple", "calories": "25"}];
+  return mealDay;
+};
---
+++ test/total-calories-test.js
@@ -0,0 +1,17 @@
+  describe('#total-calories', function() {
+    var $;
+
+    before(function() {
+      $ = document.getElementById("diary-frame").contentWindow.$;
+      localStorage.clear();
+      var mealDay = createMealDay();
+      localStorage.setItem("food_by_days_and_meal", JSON.stringify({"2/14/2007": mealDay}));
+    })
+    
+    context('meal with food', function() {
+      it ('calculates the total calories for a meal', function () {
+        var totalBreakfastCalories = $('.breakfast-list tfoot .total-calories .value').text();
+        assert.equal(totalBreakfastCalories, "136")
+      });
+    });
+  })
