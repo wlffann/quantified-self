b1d9d9e0a860265d41d88d5c09e0ed0bd680cd9a
David Knott <david1k1nott@gmail.com>
2017-03-16 12:57:08 -0600

    add changing to day functionality to localStorage
    141617749
    
    All tests are passing now but they're still dependent on one another because I'm still having trouble with beforeEach

--- test/date-test.js
+++ test/date-test.js
@@ -9,9 +9,13 @@ describe("#date-carousel", function() {
     $ = document.getElementById("diary-frame").contentWindow.$;
   });
 
-  beforeEach(function() {
+  afterEach(function() {
     var currentDate = moment().format("MMMM Do, YYYY");
     $('.current-date').html(currentDate);
+  });
+
+  after(function(){
+    localStorage.removeItem("foods_by_days_and_meals")
   });
 
   context("When a user visits the index page", function(){
@@ -37,7 +41,7 @@ describe("#date-carousel", function() {
 
   context("When a user visits clicks on the foreward button", function(){
     it ("they see tommorows date", function(){
-      $(".day-foreward").click();
+      $(".day-forward").click();
       var actualCurrentDate = $(".current-date").text().trim();
       var expectedCurrentDate = moment().add(1, 'days').format("MMMM Do, YYYY");
       assert.equal(actualCurrentDate, expectedCurrentDate)
@@ -51,14 +55,14 @@ describe("#date-carousel", function() {
       var breakfast = meals["breakfast"];
 
 
-      assert.equal(get_length(dates), 1);
+      assert.equal(get_length(dates), 2);
       assert.equal(get_length(meals), 4);
       assert.equal(breakfast.length, 0);
     });
 
 
     it ("they can see 2 days ahead", function(){
-      $(".day-foreward").click();
+      $(".day-forward").click();
       var actualCurrentDate = $(".current-date").text().trim();
       var expectedCurrentDate = moment().add(2, 'days').format("MMMM Do, YYYY");
       assert.equal(actualCurrentDate, expectedCurrentDate)
@@ -71,6 +75,7 @@ describe("#date-carousel", function() {
       $(".day-backward").click();
       $(".day-backward").click();
       $(".day-backward").click();
+      
       
       var actualCurrentDate = $(".current-date").text().trim();
       var expectedCurrentDate = moment().add(-1, 'days').format("MMMM Do, YYYY");
@@ -85,7 +90,7 @@ describe("#date-carousel", function() {
       var breakfast = meals["breakfast"];
 
 
-      assert.equal(get_length(dates), 1);
+      assert.equal(get_length(dates), 4);
       assert.equal(get_length(meals), 4);
       assert.equal(breakfast.length, 0);
     });
@@ -97,17 +102,4 @@ describe("#date-carousel", function() {
       assert.equal(actualCurrentDate, expectedCurrentDate)
     });
   });
-
-  // context("When a user clicks on the foreward button", function(){
-  //   it("they see the foods they will eat on the next day", function(){
-  //     var currentDate = moment().add(1, 'days').format("MMMM Do, YYYY")
-  //     var datesJSON = localStorage.getItem("foods_by_days_and_meals");
-  //     var dates = 
-  //     if(foodItemsJSON === null){
-  //   foodItemsJSON = '[]';
-  // }
-  // var currentFoodItems = JSON.parse(foodItemsJSON)
-  //     var foods = dates.
-  //   })
-  // });
 });
